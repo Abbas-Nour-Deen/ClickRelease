eec646f9da767e47df03973dec9b14bc02f8333f
unknown <a.nourdeen>
2024-08-03 20:42:18 +0300

    fix service issue

--- lib/models/service_model.dart
+++ lib/models/service_model.dart
@@ -1,11 +1,8 @@
-import 'dart:typed_data';
-
 class ServiceModel {
   final String serviceId;
   final String catId;
   final String nameEn;
   final String descr;
-  final Uint8List image;
   final String nameAr;
 
   ServiceModel({
@@ -13,19 +10,15 @@ class ServiceModel {
     required this.catId,
     required this.nameEn,
     required this.descr,
-    required this.image,
     required this.nameAr,
   });
 
   factory ServiceModel.fromJson(Map<String, dynamic> json) {
-    List<dynamic> data = json['serviceIcon']['data'];
-    List<int> bytes = data.cast<int>();
     return ServiceModel(
       serviceId: json['serviceId'],
       catId: json['catId'] ?? '',
       nameEn: json['serviceNameEng'] ?? '',
       descr: json['description'] ?? '',
-      image: Uint8List.fromList(bytes),
       nameAr: json['serviceNameArb'] ?? '',
     );
   }
